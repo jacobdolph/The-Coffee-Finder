f2ae2b6d9aab36ce60fd49eb028b85ac08949df3
Jacob Dolph <jacob.dolph702@gmail.com>
2020-06-02 23:12:28 -0600

    added a background image

--- javascript/script.js
+++ javascript/script.js
@@ -84,6 +84,7 @@ getCoffeeShops = (lat, lon) => {
                         layers: L.mapquest.tileLayer('map'),
                         zoom: 13
                     });
+                    L.mapquest.geocodingControl().addTo(map)
                     L.mapquest.directions().route({
                         start: startAddress,
                         end: shopAddress
@@ -123,20 +124,14 @@ const findByZipcode = (pos) => {
         //the lat lon data returned from this query is flipped
         let lat = zips.records[0].fields.longitude
         let lon = zips.records[0].fields.latitude
-        // var queryURL = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?";
         let queryURL = buildQueryUrl(lat, lon);
-
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-
             responseEl = response;
-
             for (var i = 0; i < 4; i++) {
-
                 let cityEl = responseEl.candidates[i].attributes.Place_addr;
-
                 let coffeeShopCard = $("<div>").attr({
                     "class": "card-button col s12 m3",
                     "data-address": response.candidates[i].attributes.Place_addr
@@ -155,11 +150,9 @@ const findByZipcode = (pos) => {
                 let cardContent = $("<div>").addClass("card-content card-items").attr({
                     "style": "padding-left:0"
                 })
-
                 let cardIdentifier = $("<h6>").text("Coffee Shop");
                 let cardStoreHours = $("<h6>").text("Store Hours: 6am to 9pm");
                 buildCoffeeShopCard(coffeeShopCard, horizontalCard, cardTitle, cardStack, cardContent, cardIdentifier, cardImg, cardStoreHours);
-
             }
             $.ajax({
                 url: reverseURL,
